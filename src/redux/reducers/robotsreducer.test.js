3f587c9816ea2c632501db732a7e6f867d96f4d8
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-06 17:23:40 +0100

    Tested default action robotsReducer

--- src/redux/reducers/robotsreducer.test.js
+++ src/redux/reducers/robotsreducer.test.js
@@ -29,4 +29,27 @@ describe("Given a robotsReducer reducer", () => {
       expect(newRobotsList).toEqual(robots);
     });
   });
+  describe("When it receives a robots list and an unknown action", () => {
+    test("Then it should return the received robots list", () => {
+      const initialRobots = [
+        {
+          _id: "1",
+          name: "Wall-e",
+          url: "http://macmagazine.com.br/blog/wp-content/uploads/2008/06/28-walle021.jpg",
+          features: {
+            speed: 2,
+            stamina: 10,
+            creationDate: new Date(),
+          },
+        },
+      ];
+      const action = {
+        type: "anything",
+      };
+
+      const newRobotsList = robotsReducer(initialRobots, action);
+
+      expect(newRobotsList).toEqual(initialRobots);
+    });
+  });
 });
