2a0cc1369049a2aec0e848decca09ab009aaff11
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-06 18:03:59 +0100

    Created Robot component

--- src/redux/thunks/robotsThunks.js
+++ src/redux/thunks/robotsThunks.js
@@ -1,9 +1,9 @@
 import { loadRobotsAction } from "../actions/actionCreators";
 
-// const urlApi = process.env.REACT_APP_URL_API;
+const urlApi = process.env.REACT_APP_URL_API;
 
 export const loadRobotsThunk = () => async (dispatch) => {
-  const response = await fetch("https://app-robots.herokuapp.com/robots");
+  const response = await fetch(urlApi);
   const robots = await response.json();
   dispatch(loadRobotsAction(robots));
 };
