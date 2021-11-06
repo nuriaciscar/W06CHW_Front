e66a581a6efe78cdfccad6d49c8dd5ebca523ebd
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-06 20:07:17 +0100

    Added test-utils render at all the tests

--- src/components/FormCreate/FormCreate.test.js
+++ src/components/FormCreate/FormCreate.test.js
@@ -4,7 +4,7 @@ import { screen } from "@testing-library/react";
 
 describe("Given a component FormCreate", () => {
   describe("When it's rendered'", () => {
-    test("Then it shoul render a form with a button create", () => {
+    test("Then it should render a form with a button create", () => {
       renderWithProviders(<FormCreate />);
 
       const elementForm = screen.getByRole("button", { name: /create/i });
--- src/components/RobotList/RobotList.test.js
+++ src/components/RobotList/RobotList.test.js
@@ -1,7 +1,7 @@
 import renderWithProviders from "../../utils/test-utils";
 import RobotList from "./RobotList";
 
-describe("Given a component Robot", () => {
+describe("Given a component RobotList component", () => {
   describe("When it's rendered'", () => {
     test("Then it renders a list of robots", () => {
       renderWithProviders(<RobotList />);
