5b0c1f71af8fe61960944dcf674e191f600aa42c
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-07 18:00:20 +0100

    Created loadRoboAction on actionCreators

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -4,3 +4,8 @@ export const loadRobotsAction = (robots) => ({
   type: actionTypes.loadRobots,
   robots,
 });
+
+export const loadRobotAction = (robot) => ({
+  type: actionTypes.loadRobot,
+  robot,
+});
