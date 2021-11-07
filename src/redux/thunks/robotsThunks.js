4740999087f22fb66bc7868ad61cb1f8474be077
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-07 18:58:36 +0100

    Created deleteRobotsThunk

--- src/redux/thunks/robotsThunks.js
+++ src/redux/thunks/robotsThunks.js
@@ -1,4 +1,7 @@
-import { loadRobotsAction } from "../actions/actionCreators";
+import {
+  deleteRobotsAction,
+  loadRobotsAction,
+} from "../actions/actionCreators";
 
 // const urlApi = process.env.REACT_APP_URL_API;
 
@@ -8,4 +11,17 @@ export const loadRobotsThunk = () => {
     const robots = await response.json();
     dispatch(loadRobotsAction(robots));
   };
+};
+
+export const deleteRobotsThunk = (id) => async (dispatch) => {
+  const response = await fetch(
+    `https://app-robots2.herokuapp.com/robots/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+
+  if (response.ok) {
+    dispatch(deleteRobotsAction(id));
+  }
 };
