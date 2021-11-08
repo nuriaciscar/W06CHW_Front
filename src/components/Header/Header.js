7e9975a371ecbd8e155b44cb0dd8a2410db2c01f
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-08 18:27:00 +0100

    Added userReducer on index reducers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,26 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const { isAuthenticated } = useSelector(({ user }) => user);
   return (
     <main className="container">
       <header class="d-flex justify-content-center py-3">
-        <NavLink to="/" exact className="nav-link active">
-          HOME
-        </NavLink>
-        <NavLink
-          to="/robots/create"
-          exact
-          className="nav-link "
-          aria-current="page"
-        >
-          CREATE ROBOT
-        </NavLink>
+        (isAuthenticated ? (
+        <>
+          <NavLink to="/" exact className="nav-link active">
+            HOME
+          </NavLink>
+          <NavLink
+            to="/robots/create"
+            exact
+            className="nav-link "
+            aria-current="page"
+          >
+            CREATE ROBOT
+          </NavLink>
+        </>
+        )
       </header>
     </main>
   );
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import robotsReducer from "./robotsReducer";
 import { combineReducers } from "redux";
+import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   robots: robotsReducer,
+  userReducer,
 });
 
 export default rootReducer;
